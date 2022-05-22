b1747769f354b0f4681c86303092d8185efe5a9d
TiltedToast <timdortmann@gmail.com>
2022-05-22 22:06:24 +0200

    help msg when no args

--- main.go
+++ main.go
@@ -17,12 +17,7 @@ type inputOptions struct {
 func main() {
 	args := os.Args[1:]
 
-	if len(args) == 0 {
-		fmt.Println("No arguments provided")
-		return
-	}
-
-	if contains(args, "-h") || contains(args, "--help") {
+	if contains(args, "-h") || contains(args, "--help") || len(args) == 0 {
 		printHelpMessage()
 		return
 	}
@@ -51,12 +46,12 @@ func printHelpMessage() {
 	fmt.Println("  danbooru-dl [options] <tags>")
 	fmt.Println("")
 	fmt.Println("Options:")
-	fmt.Println("  -h, --help     print this help message and exit")
-	fmt.Println("  -o, --output   output directory, defaults to 'output' subdirectory")
-	fmt.Println("  -t, --tag      the specific tag(s) you want to download, separated by commas")
-	fmt.Println("  -s, --safe     add this flag for safe images")
-	fmt.Println("  -r, --risky    add this flag for suggestive images")
-	fmt.Println("  -e, --explicit add this flag for clearly 18+ images")
+	fmt.Println("  -h, --help       print this help message and exit")
+	fmt.Println("  -o, --output     output directory, defaults to 'output' subdirectory")
+	fmt.Println("  -t, --tag        the specific tag(s) you want to download, separated by commas")
+	fmt.Println("  -s, --safe       add this flag for safe images")
+	fmt.Println("  -r, --risky      add this flag for suggestive images")
+	fmt.Println("  -e, --explicit   add this flag for clearly 18+ images")
 	fmt.Println("")
 	fmt.Println("For more information, see https://github.com/TiltedToast/danbooru-dl-go")
 }
@@ -68,6 +63,7 @@ func parseArgs(args []string) inputOptions {
 	opts.explicit = false
 	opts.risky = false
 	opts.safe = false
+	opts.tags = []string{}
 
 	for i := 0; i < len(args); i++ {
 		switch args[i] {
@@ -75,14 +71,10 @@ func parseArgs(args []string) inputOptions {
 			if len(args) > i+1 {
 				opts.outputDir = args[i+1]
 			}
-			i++
 		case "-t", "--tag":
 			if len(args) > i+1 {
 				opts.tags = strings.Split(args[i+1], ",")
-			} else {
-				opts.tags = []string{}
 			}
-			i++
 		case "-r", "--risky":
 			opts.risky = true
 		case "-e", "--explicit":
