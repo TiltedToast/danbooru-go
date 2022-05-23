fff69be052aa53b1bbc4ad8754ddde2e3bb55d26
TiltedToast <57221600+TiltedToast@users.noreply.github.com>
2022-05-23 03:17:17 +0200

    fuck you and i'll see you tomorrow

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -9,8 +12,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 type inputOptions struct {
@@ -22,10 +23,51 @@ type inputOptions struct {
 }
 
 type Post struct {
-	rating   string
-	id       string
-	file_ext string
-	file_url string
+	ID                  int         `json:"id"`
+	CreatedAt           string      `json:"created_at"`
+	UploaderID          int         `json:"uploader_id"`
+	Score               int         `json:"score"`
+	Source              string      `json:"source"`
+	Md5                 string      `json:"md5"`
+	LastCommentBumpedAt interface{} `json:"last_comment_bumped_at"`
+	Rating              string      `json:"rating"`
+	ImageWidth          int         `json:"image_width"`
+	ImageHeight         int         `json:"image_height"`
+	TagString           string      `json:"tag_string"`
+	FavCount            int         `json:"fav_count"`
+	FileExt             string      `json:"file_ext"`
+	LastNotedAt         string      `json:"last_noted_at"`
+	ParentID            interface{} `json:"parent_id"`
+	HasChildren         bool        `json:"has_children"`
+	ApproverID          int         `json:"approver_id"`
+	TagCountGeneral     int         `json:"tag_count_general"`
+	TagCountArtist      int         `json:"tag_count_artist"`
+	TagCountCharacter   int         `json:"tag_count_character"`
+	TagCountCopyright   int         `json:"tag_count_copyright"`
+	FileSize            int         `json:"file_size"`
+	UpScore             int         `json:"up_score"`
+	DownScore           int         `json:"down_score"`
+	IsPending           bool        `json:"is_pending"`
+	IsFlagged           bool        `json:"is_flagged"`
+	IsDeleted           bool        `json:"is_deleted"`
+	TagCount            int         `json:"tag_count"`
+	UpdatedAt           string      `json:"updated_at"`
+	IsBanned            bool        `json:"is_banned"`
+	PixivID             int         `json:"pixiv_id"`
+	LastCommentedAt     interface{} `json:"last_commented_at"`
+	HasActiveChildren   bool        `json:"has_active_children"`
+	BitFlags            int         `json:"bit_flags"`
+	TagCountMeta        int         `json:"tag_count_meta"`
+	HasLarge            bool        `json:"has_large"`
+	HasVisibleChildren  bool        `json:"has_visible_children"`
+	TagStringGeneral    string      `json:"tag_string_general"`
+	TagStringCharacter  string      `json:"tag_string_character"`
+	TagStringCopyright  string      `json:"tag_string_copyright"`
+	TagStringArtist     string      `json:"tag_string_artist"`
+	TagStringMeta       string      `json:"tag_string_meta"`
+	FileURL             string      `json:"file_url"`
+	LargeFileURL        string      `json:"large_file_url"`
+	PreviewFileURL      string      `json:"preview_file_url"`
 }
 
 func main() {
@@ -47,13 +89,28 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(len(opts.tags))
 
+	posts := []Post{}
+	totalPages := []int{}
+
 	for _, tag := range opts.tags {
-		go func(currentTag string) {
+		go func(tag string) {
 			defer wg.Done()
-			fmt.Println(currentTag, ":", getTotalPages(currentTag))
+			tagPosts := fetchPosts(tag, totalPages)
+			posts = append(posts, tagPosts...)
 		}(tag)
 	}
 	wg.Wait()
+
+	wg2 := sync.WaitGroup{}
+	wg2.Add(len(opts.tags))
+
+	for _, tag := range opts.tags {
+		go func(currentTag string) {
+			defer wg2.Done()
+			posts = append(posts, fetchPosts(currentTag, totalPages)...)
+		}(tag)
+	}
+	wg2.Wait()
 
 	newpath := filepath.Join(".", opts.outputDir)
 	err := os.MkdirAll(newpath, os.ModePerm)
@@ -63,17 +120,60 @@ func main() {
 		return
 	}
 
+	fmt.Println(len(posts))
+
 }
 
-// func fetchPosts(args inputOptions, totalPages []int) {
-// 	posts := []Post{}
+func fetchPosts(tag string, totalPages []int) []Post {
+	posts := []Post{}
 
-// 	for _, tag := range args.tags {
-// 		for page := 1; page <= getTotalPages(tag); page++ {
-// 			posts = append(posts, getPosts(tag, page)...)
-// 		}
-// 	}
-// }
+	wg := sync.WaitGroup{}
+	wg.Add(len(totalPages))
+	for _, currentPage := range totalPages {
+		go func(currPage int) {
+			defer wg.Done()
+			posts = append(posts, fetchPostsFromPage(tag, currPage)...)
+		}(currentPage)
+	}
+	wg.Wait()
+	return posts
+}
+
+func fetchPostsFromPage(tag string, totalPageAmount int) []Post {
+	posts := []Post{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(totalPageAmount)
+
+	for i := 1; i <= totalPageAmount; i++ {
+		go func(currentPage int) {
+			defer wg.Done()
+			url := fmt.Sprintf("https://danbooru.donmai.us/posts.json?page=%d&tags=%s", currentPage, url.QueryEscape(tag))
+
+			response, err := http.Get(url)
+			if err != nil {
+				return
+			}
+
+			defer response.Body.Close()
+
+			responseData, err := ioutil.ReadAll(response.Body)
+
+			if err != nil {
+				return
+			}
+
+			var result []Post
+			if err := json.Unmarshal(responseData, &result); err != nil {
+				return
+			}
+
+			posts = append(posts, result...)
+		}(i)
+	}
+	wg.Wait()
+	return posts
+}
 
 func getTotalPages(tag string) int {
 	url := fmt.Sprintf("https://danbooru.donmai.us/posts?tags=%s", url.QueryEscape(tag))
